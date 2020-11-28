85605e2b23817780fcb65e14248a4d88e599c403
Rafael <rafaeljuliani1984@gmail.com>
2020-11-28 20:49:17 -0300

    Aula 2 - Variaveis

---
+++ php-basico/02-variaveis.php
@@ -0,0 +1,19 @@
+<?php
+
+  $nome = "Fulano Doe";
+  $idade = 33;
+  $altura = 1.75;
+
+  echo "Meu nome é $nome, tenho $idade anos e possuo $altura m. <br/>";
+
+  $nome = "Rafael Diehl";
+  $idade = 17;
+  $altura = 1.78;
+
+  echo "Meu nome é $nome, tenho $idade anos e possuo $altura m. <br/> <hr/>";
+
+  // O nome das variáveis PHP é case sensitive
+  $carro = "Hilux";
+  $Carro = "Fusca";
+
+  echo $carro;
