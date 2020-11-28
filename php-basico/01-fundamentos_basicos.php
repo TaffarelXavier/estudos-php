fe2940547a69c3349cdc92cb69f503965114f06e
Rafael <rafaeljuliani1984@gmail.com>
2020-11-28 20:40:22 -0300

    Aula 1 - Fundamentos basicos

---
+++ php-basico/01-fundamentos_basicos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Fundamentos básicos</title>
+</head>
+<body>
+    
+  <!--
+    Os comandos "echo" e "print" são usados para saída de dados. A diferença é que o "echo" não tem nenhum retorno, enquanto "print" retorna 1 para que ele possa ser usado em expressões.
+  -->
+
+  <?php
+    echo "Meu nome é Rafael <br/>";
+    print "Minha idade é 17";
+  ?>
+
+</body>
+</html>
