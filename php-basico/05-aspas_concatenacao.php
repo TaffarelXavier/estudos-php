7f913cd236dec7d5ac007c086295e2e546727219
Rafael <rafaeljuliani1984@gmail.com>
2020-11-28 21:24:40 -0300

    Aula 5 - Aspas e Concatenaçao

---
+++ php-basico/05-aspas_concatenacao.php
@@ -0,0 +1,9 @@
+<?php
+
+  $nome = "Rafael Diehl";
+  $idade = 17;
+  $linguagem = "PHP";
+
+  echo 'Meu nome é $nome. <br/>'; // Aspas simples são literais
+  echo "Minha idade é " . $idade . ". <br/>"; // "." é o operador de concatenação
+  echo 'Estou aprendendo a \'linguagem\' ' . $linguagem . '.'; // Para usar aspas dentro das aspas, usa-se "\" antes
