5c85ac9dcecc9856197e780632575f2435323aa9
Rafael <rafaeljuliani1984@gmail.com>
2020-11-30 15:32:31 -0300

    Aula 16 - Condicionais (Switch / Case)

--- php-basico/10-condicionais.php
+++ php-basico/10-condicionais.php
@@ -10,15 +10,38 @@
     echo "É diferente e maior que 10";
   }
 
+  /* Forma alternativa
+  if ($num == 10):
+    echo "É igual a 10";
+  else:
+    echo "É diferente de 10;
+  endif; */
+
   echo "<hr />";
 
   $media = 5;
 
   echo ($media >= 6) ? "Aprovado" : "Reprovado";
 
-  /* Forma alternativa
-  if ($num == 10):
-    echo "É igual a 10";
-  else:
-    echo "É diferente de 10;
-  endif;
+  echo "<hr />";
+
+  $cor = "vermelho";
+
+  switch ($cor) {
+    case "vermelho":
+      echo "Sua cor preferida é o vermelho";
+      break;
+
+    case "verde":
+      echo "Sua cor preferida é o verde";
+      break;
+
+    case "azul":
+      echo "Sua cor preferida é o azul";
+      break;
+
+    default:
+      echo "Sua cor preferida não é vermelho, verde ou azul";
+      break;
+  }
+
