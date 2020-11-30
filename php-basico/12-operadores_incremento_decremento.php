cd0214490ec59e0c2d203c7e7aea42261a3bfe92
Rafael <rafaeljuliani1984@gmail.com>
2020-11-30 15:50:06 -0300

    Aula 18 - Operadores de Incremento e Decremento

---
+++ php-basico/12-operadores_incremento_decremento.php
@@ -0,0 +1,17 @@
+<?php
+
+  $valor = 20;
+
+  echo ++$valor . "<br />"; // Pré-incremento
+
+  echo $valor++ . "<br />"; // Pós-incremento
+
+  echo $valor;
+
+  echo "<hr />";
+
+  echo --$valor . "<br />"; // Pré-decremento
+
+  echo $valor-- . "<br />"; // Pós-decremento
+
+  echo $valor;
