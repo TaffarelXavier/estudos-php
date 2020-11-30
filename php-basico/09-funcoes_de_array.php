122def28ecc7c1ab7670a37e3706bc864027fc27
Rafael <rafaeljuliani1984@gmail.com>
2020-11-29 23:42:42 -0300

    Aula 12 - Funçoes de Arrays #2

--- php-basico/09-funcoes_de_array.php
+++ php-basico/09-funcoes_de_array.php
@@ -20,3 +20,28 @@
   $usuario = ["Nome" => "Rafael", "Idade" => 17, "Telefone" => "+55 (YY) XXXXX-XXXX"];
   $keys = array_keys($usuario);
   print_r($keys);
+
+  echo "<hr />";
+
+  $valores = array_values($usuario);
+  print_r($valores);
+
+  echo "<hr />";
+
+  $carros = ["Camaro", "Uno", "Gol"];
+  $motos = ["Pop100", "50cc", "Cb1000"];
+  $veiculos = array_merge($carros, $motos);
+
+  print_r($veiculos);
+
+  echo "<hr />";
+
+  print_r($carros);
+  echo "<br /> Valor excluído: " . array_pop($carros) . "<br />";
+  print_r($carros);
+
+  echo "<hr />";
+
+  print_r($motos);
+  echo "<br /> Valor excluído: " . array_shift($motos) . "<br />";
+  print_r($motos);
