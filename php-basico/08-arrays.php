994bc6144a914de81c7a349bc444ff3adc14baf9
Rafael <rafaeljuliani1984@gmail.com>
2020-11-29 23:12:32 -0300

    Aula 10 - Arrays #3

--- php-basico/08-arrays.php
+++ php-basico/08-arrays.php
@@ -1,43 +1,72 @@
 <?php
 
-    $classes = array(1 => "Guerreiro", 2 => "Mago", 3 => "Ladino");
-    $classes[] = "Clérigo";
-    $classes[10] = "Bardo";
-    print_r($classes);
+  $classes = array(1 => "Guerreiro", 2 => "Mago", 3 => "Ladino");
+  $classes[] = "Clérigo";
+  $classes[10] = "Bardo";
+  print_r($classes);
 
-    echo "<hr />";
+  echo "<hr />";
 
-    echo $classes[3];
+  echo $classes[3];
 
-    echo "<hr />";
+  echo "<hr />";
 
-    $npc = array();
-    $npc["raça"] = "Anão";
-    $npc["classe"] = $classes[4];
-    $npc["alinhamento"] = "Leal e Bom";
+  $npc = array();
+  $npc["raça"] = "Anão";
+  $npc["classe"] = $classes[4];
+  $npc["alinhamento"] = "Leal e Bom";
 
-    print_r($npc);
+  print_r($npc);
 
-    echo "<br />";
+  echo "<br />";
 
-    $equipamento = ["arma" => "Martelo", "armadura" => "Cota de Malha", "escudo" => "Escudo de Madeira"];
+  $equipamento = ["arma" => "Martelo", "armadura" => "Cota de Malha", "escudo" => "Escudo de Madeira"];
 
-    print_r($equipamento);
+  print_r($equipamento);
 
-    echo "<hr />";
+  echo "<hr />";
 
-    echo "Raça: " . $npc['raça'] . "<br />";
-    echo "Classe: " . $npc['classe'] . "<br />";
-    echo "Alinhamento: " . $npc['alinhamento'] . "<br />";
+  echo "Raça: " . $npc['raça'] . "<br />";
+  echo "Classe: " . $npc['classe'] . "<br />";
+  echo "Alinhamento: " . $npc['alinhamento'] . "<br />";
 
-    echo "Equipamento: <ul>";
+  echo "Equipamento: <ul>";
 
-    foreach($equipamento as $item) {
-        echo "<li>$item</li>";
+  foreach($equipamento as $item) {
+      echo "<li>$item</li>";
+  }
+
+  echo "</ul> <hr />";
+
+  echo "Número de classes disponíveis: " . count($classes) . "<br />";
+  $itens = count($equipamento);
+  echo "Número de itens: $itens";
+
+  echo "<hr />";
+
+  $raças = array(
+      "Anão" => array("Anão da Montanha", "Anão da Colina", "Duergar"),
+      "Elfo" => array("Elfo da Floresta", "Alto Elfo", "Drow"),
+      "Halfling" => array("Halfling Pés-leves", "Halfling Robusto"),
+  );
+
+  $i = 0;
+  foreach($raças as $raça) {
+    $chave = array_keys($raças);
+    echo $chave[$GLOBALS['i']] . "<ul>";
+    foreach($raça as $subraça) {
+      echo "<li>$subraça</li>";
     }
+    echo "</ul>";
+    $GLOBALS['i']++;
+  }
 
-    echo "</ul> <hr />";
+  echo "<hr />";
 
-    echo "Número de classes disponíveis: " . count($classes) . "<br />";
-    $itens = count($equipamento);
-    echo "Número de itens: $itens";
+  $conjuradores = ["Carisma" => "Feiticeiro", "Sabedoria" => "Druida", "Inteligência" => "Mago"];
+
+  echo "Conjuradores <ul>";
+  foreach($conjuradores as $atributo => $classe) {
+    echo "<li>$classe (Atributo de conjuração: $atributo)</li>";
+  }
+  echo "</ul>";
