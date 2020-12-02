345ba1bad6d287ab1b0834d11a63b7dda74c100f
Rafael <rafaeljuliani1984@gmail.com>
2020-12-01 21:35:56 -0300

    Aula 25 - Funçoes para Strings #2

--- php-basico/18-funcoes_para_strings.php
+++ php-basico/18-funcoes_para_strings.php
@@ -19,3 +19,22 @@
   $nome = " Pereira";
   $novoNome = str_pad($nome, 15, "Ednaldo", STR_PAD_LEFT);
   echo $novoNome;
+
+  echo "<hr />";
+
+  $string = str_repeat("Eco ", 5);
+  echo $string;
+
+  echo "<hr />";
+
+  echo strlen($mensagem);
+
+  echo "<hr />";
+
+  $texto = "A seleção Argentina será campeã da Copa do Mundo de 2022";
+  $novoTexto = str_replace("Argentina", "Brasileira", $texto);
+  echo $novoTexto;
+
+  echo "<hr />";
+
+  echo strpos($texto, "2022");
