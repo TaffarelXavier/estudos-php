b6033baa020e77edd4064ede36c327066a561167
Rafael <rafaeljuliani1984@gmail.com>
2020-12-02 23:47:45 -0300

    Aula 34 - Upload multiplo

--- php-basico/24-upload/index.php
+++ php-basico/24-upload/index.php
@@ -11,29 +11,36 @@
 
     if(isset($_POST['enviar'])) {
       $formatosPermitidos = ["png", "jpeg", "jpg", "gif"];
-      $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
+      $quantidadeArquivos = count($_FILES['arquivo']['name']);
+      $contador = 0;
+      while ($contador < $quantidadeArquivos) {
+        $extensao = pathinfo($_FILES['arquivo']['name'][$contador], PATHINFO_EXTENSION);
 
-      if(in_array($extensao, $formatosPermitidos)) {
-        $pasta = "arquivos/";
-        $temporario = $_FILES['arquivo']['tmp_name'];
-        $novoNome = uniqid() . ".$extensao";
+        if(in_array($extensao, $formatosPermitidos)) {
+          $pasta = "arquivos/";
+          $temporario = $_FILES['arquivo']['tmp_name'][$contador];
+          $novoNome = uniqid() . ".$extensao";
 
-        if(move_uploaded_file($temporario, $pasta.$novoNome)) {
-          $mensagem = "Upload feito com sucesso!";
+          if(move_uploaded_file($temporario, $pasta.$novoNome)) {
+            echo "Upload feito com sucesso para $pasta.$novoNome <br />";
+          } else {
+            echo "Erro ao enviar o arquivo $temporario";
+          }
         } else {
-          $mensagem = "Erro, não foi possível fazer o upload";
+          echo "O formato $extensao não é permitido <br /> Formato permitidos: ";
+          foreach($formatosPermitidos as $formato) {
+            echo "<li>$formato</li>";
+          }
         }
-      } else {
-        $mensagem = "Formato não permitido";
-      }
 
-      echo $mensagem;
+        $contador++;
+      }
     }
 
   ?>
 
   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-    <input type="file" name="arquivo" /> <br />
+    <input type="file" name="arquivo[]" multiple /> <br />
     <button typo="submit" name="enviar">
       Enviar arquivo
     </button>
