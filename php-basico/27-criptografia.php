d0d94070b83002922eafc01a7c9a7d2deca0d3d7
Rafael <rafaeljuliani1984@gmail.com>
2020-12-05 00:35:16 -0300

    Aula 38 - Senhas seguras com password hash

--- php-basico/27-criptografia.php
+++ php-basico/27-criptografia.php
@@ -1,6 +1,6 @@
 <?php
 
-  $senha = "love";
+  $senha = "123456";
   echo "Senha: $senha";
 
   echo "<hr />";
@@ -8,3 +8,19 @@
   echo "base64: " . base64_encode($senha) . "<br />";
   echo "MD5: " . md5($senha) . "<br />";
   echo "Sha1: " . sha1($senha);
+
+  echo "<hr />";
+
+  $options = ['cost' => 10];
+  $senhaSegura = password_hash($senha, PASSWORD_DEFAULT, $options);
+  echo "Hash: $senhaSegura";
+
+  echo "<hr />";
+
+  $senha_db = "$2y$10\$rmqWXCKJ2W3pC7fjUTdOtejD1yucN2toswr5l/Jqm0m6wECnC0wRq";
+
+  if(password_verify($senha, $senha_db)) {
+    echo "Senha válida";
+  } else {
+    echo "Senha inválida";
+  }
