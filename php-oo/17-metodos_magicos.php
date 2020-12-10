b9b255aa9eb43588478268cca44c43f14e5eb34c
Rafael <rafaeljuliani1984@gmail.com>
2020-12-10 19:41:50 -0300

    Aula 18 - Metodos Magicos

---
+++ php-oo/17-metodos_magicos.php
@@ -0,0 +1,33 @@
+<?php
+
+  class Pessoa {
+    private $dados = array();
+
+    public function __set($nome, $valor) {
+      $this->dados[$nome] = $valor;
+    }
+
+    public function __get($nome) {
+      return $this->dados[$nome];
+    }
+
+    public function __toString() {
+      return "Tentei imprimir o objeto!";
+    }
+
+    public function __invoke() {
+      return "Objeto como função!";
+    }
+  }
+
+  $pessoa = new Pessoa();
+  $pessoa->nome = "Rafael";
+  $pessoa->idade = 17;
+  $pessoa->sexo = "M";
+
+  echo $pessoa->nome . "<br />";
+  echo $pessoa->idade . "<br />";
+  echo $pessoa->sexo . "<br />";
+
+  echo $pessoa . "<br />";
+  echo $pessoa();
