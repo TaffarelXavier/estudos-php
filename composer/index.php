af2365024aca1a76a3aaf003e6c489c661a64cf5
Rafael <rafaeljuliani1984@gmail.com>
2020-12-10 20:36:55 -0300

    Aula 2 - Composer Install

--- composer/index.php
+++ composer/index.php
@@ -1,3 +1,9 @@
 <?php
 
-  echo "Olá mundo!";
+  require_once 'vendor/autoload.php';
+
+  use Cocur\Slugify\Slugify;
+
+  $slugify = new Slugify();
+  $slugify->addRule('ão', 'funcionou');
+  echo $slugify->slugify('Olá mundo! ão', '_');
